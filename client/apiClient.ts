a0881e0c24873c5a37d2243d5712de577d646e19
edward-soung-24 <170850092+EddieWeddie11@users.noreply.github.com>
2024-08-15 17:35:33 +1200

    Added API and DC Function

--- client/apiClient.ts
+++ client/apiClient.ts
@@ -1,6 +1,13 @@
 import request from 'superagent'
+import { Character } from '../models'
 
-export async function getGreeting(): Promise<string> {
-  const res = await request.get('/api/v1/greeting')
-  return res.body.greeting
+// export async function getGreeting(): Promise<string> {
+//   const res = await request.get('/api/v1/greeting')
+//   return res.body.greeting
+// }
+
+export async function getDcCharacter(): Promise<string> {
+  const res = await request.get('https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json')
+  const dcOnly = res.body.filter((dc: Character)=> dc.biography.publisher === "DC Comics")
+  return dcOnly
 }
